0cab129ebed9ad57a6cdaf85d5dc0e7fd46936d5
baibnayl <70564130+baibnayl@users.noreply.github.com>
2026-04-23 17:53:09 +0600

    Update app.run

--- app.py
+++ app.py
@@ -924,4 +924,7 @@ def report_pdf():
 
 
 if __name__ == "__main__":
-    app.run(debug=False)
+    app.run(debug=False,
+           host="0.0.0.0",
+            port=int(os.environ.get("PORT", 8080))
+    )
